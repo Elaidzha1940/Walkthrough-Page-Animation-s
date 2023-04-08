c122bdb1d40a38452bbe157b20e599381d9f5f13
Elaidzha1940 <eli080293@gmail.com>
2023-04-08 20:09:44 +0700

    Walkthrough Page Animation's + added two New Groups anf one File

---
+++ Walkthrough Page Animation's/Walkthrough Page Animation's/Model/Intro.swift
@@ -0,0 +1,20 @@
+//
+//  Intro.swift
+//  Walkthrough Page Animation's
+//
+//  Created by Elaidzha Shchukin on 08.04.2023.
+//
+
+import SwiftUI
+
+struct Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Intro_Previews: PreviewProvider {
+    static var previews: some View {
+        Intro()
+    }
+}
