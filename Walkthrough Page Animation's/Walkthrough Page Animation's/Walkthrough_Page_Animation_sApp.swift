18ca9ff0d5803c8f9150f474fbcd4ada01613f6c
Elaidzha1940 <eli080293@gmail.com>
2023-04-08 20:04:12 +0700

    Walkthrough Page Animation's

---
+++ Walkthrough Page Animation's/Walkthrough Page Animation's/Walkthrough_Page_Animation_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walkthrough_Page_Animation_sApp.swift
+//  Walkthrough Page Animation's
+//
+//  Created by Elaidzha Shchukin on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Walkthrough_Page_Animation_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
